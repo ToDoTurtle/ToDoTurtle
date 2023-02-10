c5dcbf68fd652813dc8876bdc3e45f999c61c972
FerranAD <farandomingo@gmail.com>
2023-02-10 13:58:18 +0100

    feat: add search reminder use case #3

---
+++ src/main/kotlin/reminder/application/ReminderSearcher.kt
@@ -0,0 +1,19 @@
+package reminder.application
+
+import reminder.domain.*
+import java.util.*
+
+class ReminderSearcher(
+    private val repository: ReminderRepository,
+) {
+
+    /***
+     * Searches a Reminder with the given identifier and returns it if found.
+     * @see Reminder
+     * @return An optional of a reminder instance.
+     */
+    fun search(identifier: ReminderIdentifier): Optional<Reminder> {
+        return repository.search(identifier)
+    }
+
+}
