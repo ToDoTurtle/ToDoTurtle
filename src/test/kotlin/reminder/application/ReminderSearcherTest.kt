50b9022d7269043224617d2be1c3c2241ea2a065
FerranAD <farandomingo@gmail.com>
2023-02-11 11:54:35 +0100

    feat(reminder-searcher): clean tests #3
    Remove unused variable and improve readability of test cases.

--- src/test/kotlin/reminder/application/ReminderSearcherTest.kt
+++ src/test/kotlin/reminder/application/ReminderSearcherTest.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import reminder.domain.Reminder
-import reminder.domain.ReminderIdentifier
 import reminder.domain.ReminderIdentifierGenerator
 import reminder.domain.ReminderRepository
 import reminder.mothers.IdentifierMother
@@ -13,7 +12,6 @@ import java.util.*
 import kotlin.test.assertEquals
 
 class ReminderSearcherTest {
-    private lateinit var reminderSaver: ReminderSaver
     private lateinit var generator: ReminderIdentifierGenerator
     private lateinit var reminderSearcher: ReminderSearcher
     private lateinit var repository: ReminderRepository
@@ -23,31 +21,28 @@ class ReminderSearcherTest {
         repository = Mockito.mock(ReminderRepository::class.java)
         generator = Mockito.mock(ReminderIdentifierGenerator::class.java)
         reminderSearcher = ReminderSearcher(repository)
-        reminderSaver = ReminderSaver(repository, generator)
     }
 
     @Test
     fun `Nothing is returned when valid identifier doesn't exist in the repository`() {
+        // Initialize
         val identifier = IdentifierMother.getValidIdentifier()
-        `Assert that the reminder was not found in the repository given the identifier`(identifier)
-    }
-
-    @Test
-    fun `Reminder is returned when identifier exists in the repository`() {
-        val reminder = ReminderMother.getValidReminderWithoutDescription()
-        `Assert that the reminder was found in the repository given the identifier from`(reminder)
-    }
-
-    private fun `Assert that the reminder was not found in the repository given the identifier`(identifier: ReminderIdentifier) {
         Mockito.`when`(repository.search(identifier)).thenReturn(Optional.empty())
+        // Execute
         val result = reminderSearcher.search(identifier)
+        // Assert
         Mockito.verify(repository, Mockito.times(1)).search(identifier)
         assertEquals(Optional.empty<Reminder>(), result)
     }
 
-    private fun `Assert that the reminder was found in the repository given the identifier from`(reminder: Reminder) {
+    @Test
+    fun `Reminder is returned when identifier exists in the repository`() {
+        // Initialize
+        val reminder = ReminderMother.getValidReminderWithoutDescription()
         Mockito.`when`(repository.search(reminder.id)).thenReturn(Optional.of(reminder))
+        // Execute
         val result = reminderSearcher.search(reminder.id)
+        // Assert
         Mockito.verify(repository, Mockito.times(1)).search(reminder.id)
         assertEquals(Optional.of(reminder), result)
     }
