3e47a649a3a42298c255b7154eeb29a9eee531f2
Oriol Agost Batalla <oriolagobat@gmail.com>
2023-02-13 22:11:36 +0100

    feat: add update reminder use case #5

---
+++ src/main/kotlin/note/application/NoteUpdater.kt
@@ -0,0 +1,16 @@
+package note.application
+
+import note.domain.Note
+import note.domain.NoteIdentifier
+import note.domain.NoteIdentifierGenerator
+import note.domain.NoteRepository
+
+class ReminderUpdater(
+    private val repository: NoteRepository,
+    private val identifierGenerator: NoteIdentifierGenerator,
+) {
+    fun update(identifier: NoteIdentifier, newTitle: String, newDescription: String?): Note {
+        NoteDeleter(repository).delete(identifier)
+        return NoteSaver(repository, identifierGenerator).save(newTitle, newDescription)
+    }
+}
---
+++ src/test/kotlin/note/application/NoteUpdaterTest.kt
@@ -0,0 +1,65 @@
+package note.application
+
+import note.domain.*
+import note.domain.exceptions.NonExistentNoteException
+import note.mothers.IdentifierMother
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito
+
+class ReminderUpdaterTest {
+    private lateinit var reminderUpdater: ReminderUpdater
+    private lateinit var generator: NoteIdentifierGenerator
+    private lateinit var repository: NoteRepository
+
+    private val newTitle = "title"
+    private val newDescription = "description"
+    private val identifier = IdentifierMother.getValidIdentifier()
+
+    @BeforeEach
+    fun setUp() {
+        repository = Mockito.mock(NoteRepository::class.java)
+        generator = Mockito.mock(NoteIdentifierGenerator::class.java)
+        reminderUpdater = ReminderUpdater(repository, generator)
+    }
+
+    @Test
+    fun `Invalid identifier throws NonExistentReminderException and doesn't call save`() {
+        // Initialize
+        val identifier = IdentifierMother.getValidIdentifier()
+        Mockito.`when`(repository.search(identifier)).thenReturn(null)
+
+        // Execute and Assert
+        assertThrows<NonExistentNoteException> {
+            reminderUpdater.update(identifier, newTitle, newDescription)
+        }
+
+        // Assert number of calls
+        assertNumberOfCalls(1, 0, 0)
+    }
+
+    @Test
+    fun `Existing note gets deleted and saved`() {
+        // Initialize
+        Mockito.`when`(repository.search(identifier)).thenReturn(Note(identifier, Title("title"), Description("description")))
+        Mockito.`when`(generator.generate()).thenReturn(identifier)
+
+        // Execute and Assert
+        reminderUpdater.update(identifier, newTitle, newDescription)
+
+        // Assert number of calls
+        assertNumberOfCalls(1, 1, 1)
+    }
+
+    private fun assertNumberOfCalls(
+        searchCalls: Int,
+        deleteCalls: Int,
+        saveCalls: Int,
+    ) {
+        Mockito.verify(repository, Mockito.times(searchCalls)).search(identifier)
+        Mockito.verify(repository, Mockito.times(deleteCalls)).delete(identifier)
+        Mockito.verify(repository, Mockito.times(saveCalls))
+            .save(Note(identifier, Title(newTitle), Description(newDescription)))
+    }
+}
