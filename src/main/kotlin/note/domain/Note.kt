2b920c8bf22cb574e195a6b426077336d9660f25
Oriol Agost Batalla <oriolagobat@gmail.com>
2023-02-14 20:59:21 +0100

    fix: remove redundant method

--- src/main/kotlin/note/domain/Note.kt
+++ src/main/kotlin/note/domain/Note.kt
@@ -1,5 +1,3 @@
 package note.domain
 
-data class Note(val id: NoteIdentifier, val title: Title, val description: Description?) {
-    fun toPrimitives() = NotePrimitives(id, title.title, description?.description)
-}
+data class Note(val id: NoteIdentifier, val title: Title, val description: Description?)
