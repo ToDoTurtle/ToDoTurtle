fde1369480f434c8f802be045577cb05e85c0fef
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-02-23 22:26:42 +0100

    refactor: use logic of create note on NoteUpdater

--- src/main/kotlin/note/application/NoteUpdater.kt
+++ src/main/kotlin/note/application/NoteUpdater.kt
@@ -6,7 +6,6 @@ import note.domain.NoteRepository
 import note.domain.exceptions.NonExistentNoteException
 import note.domain.exceptions.UnchangedNoteException
 import shared.domain.Identifier
-import shared.domain.exceptions.AlreadyUsedIdentifierException
 
 class NoteUpdater(
     private val repository: NoteRepository,
@@ -14,15 +13,14 @@ class NoteUpdater(
     fun update(oldNoteIdentifier: String, newNote: NotePrimitives): Note {
         val oldNoteId = Identifier(oldNoteIdentifier)
         val oldNote = repository.search(oldNoteId) ?: throw NonExistentNoteException(oldNoteId)
-        val title = oldNote.toPrimitives().title
-        val desc = oldNote.toPrimitives().description
-        if (newNote.title == title && newNote.description == desc) throw UnchangedNoteException(oldNoteId)
-        checkIfSameUuid(newNote, oldNoteIdentifier)
-        repository.delete(oldNoteId)
-        return NoteSaver(repository).save(newNote)
+        val oldNotePrimitives = oldNote.toPrimitives()
+        if (newNote.hasSameContent(oldNotePrimitives)) throw UnchangedNoteException(oldNoteId)
+        return update(oldNoteId = oldNoteId, newNote = newNote)
     }
 
-    private fun checkIfSameUuid(newNote: NotePrimitives, oldNoteIdentifier: String) {
-        if (newNote.noteId == oldNoteIdentifier) throw AlreadyUsedIdentifierException()
+    private fun update(oldNoteId: Identifier, newNote: NotePrimitives): Note {
+        val result = NoteSaver(repository).save(newNote)
+        repository.delete(oldNoteId)
+        return result
     }
 }
--- src/main/kotlin/note/domain/NotePrimitives.kt
+++ src/main/kotlin/note/domain/NotePrimitives.kt
@@ -1,3 +1,5 @@
 package note.domain
 
-data class NotePrimitives(val noteId: String, val title: String, val description: String?)
+data class NotePrimitives(val noteId: String, val title: String, val description: String?) {
+    fun hasSameContent(other: NotePrimitives) = this.title == other.title && this.description == other.description
+}
