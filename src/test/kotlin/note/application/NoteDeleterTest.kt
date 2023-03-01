791d3c3d0b44136e3f1e93ad8d16b9e4660b1c1e
Oriol Agost Batalla <oriolagobat@gmail.com>
2023-03-01 14:38:25 +0100

    refactor: test works with primitives, change delete from domain primitive

--- src/test/kotlin/note/application/NoteDeleterTest.kt
+++ src/test/kotlin/note/application/NoteDeleterTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.mockito.Mockito
+import shared.mothers.IdentifierMother
 
 class NoteDeleterTest {
     private lateinit var noteDeleter: NoteDeleter
@@ -21,12 +22,12 @@ class NoteDeleterTest {
 
     @Test
     fun `Invalid identifier throws NonExistentNoteException`() {
-        val note = NoteMother.getValidNoteWithDescription()
-        val id = NoteMother.getIdentifierFrom(note)
+        val id = IdentifierMother.getValidIdentifier()
+        val idPrimitive = IdentifierMother.getPrimitiveFrom(id)
         Mockito.`when`(repository.search(id)).thenReturn(null)
 
         assertThrows<NonExistentNoteException> {
-            noteDeleter.delete(id)
+            noteDeleter.delete(idPrimitive)
         }
 
         Mockito.verify(repository, Mockito.times(0)).delete(id)
@@ -35,10 +36,11 @@ class NoteDeleterTest {
     @Test
     fun `Existing note gets deleted from the repository`() {
         val note = NoteMother.getValidNoteWithoutDescription()
+        val idPrimitive = IdentifierMother.getPrimitiveFrom(note.id)
         Mockito.`when`(repository.search(note.id)).thenReturn(note)
 
         assertDoesNotThrow {
-            noteDeleter.delete(note.id)
+            noteDeleter.delete(idPrimitive)
         }
 
         Mockito.verify(repository, Mockito.times(1)).delete(note.id)
