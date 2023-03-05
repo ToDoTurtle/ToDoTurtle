87a74b63d7e40b9b24d81dbc4e4598aba68a25a6
FerranAD <farandomingo@gmail.com>
2023-03-05 10:29:29 +0100

    doc: add invalid identifier exception

--- src/main/kotlin/note/application/NoteRemover.kt
+++ src/main/kotlin/note/application/NoteRemover.kt
@@ -4,6 +4,7 @@ import note.domain.Note
 import note.domain.NoteRepository
 import note.domain.exceptions.NonExistentNoteException
 import shared.domain.Identifier
+import shared.domain.exceptions.InvalidUUIDException
 
 class NoteRemover(
     private val repository: NoteRepository,
@@ -11,6 +12,7 @@ class NoteRemover(
 
     /***
      * Deletes a Note with the given identifier and returns true if found.
+     * @throws InvalidUUIDException if the note id from the primitives isn't valid
      * @throws NonExistentNoteException if the note does not exist or is not saved on the repository.
      * @see Note
      */
