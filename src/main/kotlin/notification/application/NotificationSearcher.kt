c69070c97c10c5c32b3caaf9472c993f14f9281e
FerranAD <farandomingo@gmail.com>
2023-03-05 10:25:40 +0100

    doc(get-notifications): add thrown exception details

--- src/main/kotlin/notification/application/NotificationSearcher.kt
+++ src/main/kotlin/notification/application/NotificationSearcher.kt
@@ -4,12 +4,14 @@ import note.domain.Note
 import notification.domain.Notification
 import notification.domain.NotificationRepository
 import shared.domain.Identifier
+import shared.domain.exceptions.InvalidUUIDException
 
 class NotificationSearcher(
     private val repository: NotificationRepository,
 ) {
     /***
      * Looks for all Notifications with the given Note identifier in the repository.
+     * @throws InvalidUUIDException if the note identifier isn't in a valid UUID format.
      * @see Notification
      * @see Note
      * @return A collection of Notification instances (empty if none found)
